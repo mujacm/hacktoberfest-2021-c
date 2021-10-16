dba10999d4ffeedba3def947d8b06890fcb00195
Samarth Krishna <samarthkrishna09@gmail.com>
2021-10-17 02:07:17 +0530

    fibonacci series

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+fibonacci
+public class FibonacciExample {
+ 
+	public static void main(String[] args) 
+	{
+		
+		 int maxNumber = 10; 
+		 int previousNumber = 0;
+		 int nextNumber = 1;
+		 
+	        System.out.print("Fibonacci Series of "+maxNumber+" numbers:");
+ 
+	        for (int i = 1; i <= maxNumber; ++i)
+	        {
+	            System.out.print(previousNumber+" ");
+                 int sum = previousNumber + nextNumber;
+	            previousNumber = nextNumber;
+	            nextNumber = sum;
+	        }
+	}
+}
