4d48843d4e2346d2e073c97dce9b42572717dfee
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2021-10-26 17:26:07 +0530

    Create gcd.cpp

---
+++ Contributions/harshmishra/gcd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int m,n;
+ cout<<"Enter two no.s "<<endl;
+ cin>>m>>n;
+while(m!=n)
+ {
+ if(m>n)
+ m=m-n;
+ else
+ n=n-m;
+ }
+ cout<<"GCD is "<<n;
+return 0;
+}
